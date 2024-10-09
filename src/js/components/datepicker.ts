b22aad9f594b45f65db983a6e3672572d90d5313
qp0kycHuk <serega990112@mail.ru>
2024-10-09 12:15:55 +0300

    add polyfill

--- src/js/components/datepicker.ts
+++ src/js/components/datepicker.ts
@@ -1,31 +1,38 @@
 import AirDatepicker from 'air-datepicker'
 import localeRu from 'air-datepicker/locale/ru'
 
+function initDatePicker() {
+  // @ts-ignore
+  const target = this as Datepicker
+
+  const minDateAttr = target.getAttribute('mindate') || ''
+  const minDate = isNaN(Date.parse(minDateAttr)) ? false : Date.parse(minDateAttr)
+  const maxDateAttr = target.getAttribute('maxdate') || ''
+  const maxDate = isNaN(Date.parse(maxDateAttr)) ? false : Date.parse(maxDateAttr)
+
+  const dateFormat = target.getAttribute('dateformat') || 'dd.MM.yyyy'
+
+  const selectedDates = (target.getAttribute('selecteddates') || target.value || target.getAttribute('value') || '')
+    .split(',')
+    .map(Date.parse)
+    .filter((value) => !isNaN(value))
+
+  target.datePicker = new AirDatepicker(target, {
+    locale: localeRu,
+    isMobile: true,
+    autoClose: true,
+    minDate,
+    maxDate,
+    dateFormat,
+    selectedDates,
+  })
+}
+
 class Datepicker extends HTMLInputElement {
   datePicker: AirDatepicker | null = null
 
   connectedCallback() {
-    const minDateAttr = this.getAttribute('mindate') || ''
-    const minDate = isNaN(Date.parse(minDateAttr)) ? false : Date.parse(minDateAttr)
-    const maxDateAttr = this.getAttribute('maxdate') || ''
-    const maxDate = isNaN(Date.parse(maxDateAttr)) ? false : Date.parse(maxDateAttr)
-
-    const dateFormat = this.getAttribute('dateformat') || 'dd.MM.yyyy'
-
-    const selectedDates = (this.getAttribute('selecteddates') || this.value || this.getAttribute('value') || '')
-      .split(',')
-      .map(Date.parse)
-      .filter((value) => !isNaN(value))
-
-    this.datePicker = new AirDatepicker(this, {
-      locale: localeRu,
-      isMobile: true,
-      autoClose: true,
-      minDate,
-      maxDate,
-      dateFormat,
-      selectedDates,
-    })
+    initDatePicker.call(this)
   }
 
   disconnectedCallback() {
@@ -35,6 +42,34 @@ class Datepicker extends HTMLInputElement {
 
 function register() {
   customElements.define('air-datepicker', Datepicker, { extends: 'input' })
+  document.addEventListener('DOMContentLoaded', () => {
+    polyfill(document.body)
+
+    const observer = new MutationObserver((mutations, b) => {
+      console.log(mutations, b)
+      mutations.forEach((record) => {
+        polyfill(record.target as HTMLElement)
+      })
+    })
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    })
+  })
+  // polyfill(document.body)
+}
+
+function polyfill(container: HTMLElement) {
+  const targets = Array.from(container.querySelectorAll<Datepicker>('[is="air-datepicker"]'))
+
+  targets.forEach((target) => {
+    if (target.datePicker) {
+      return
+    }
+
+    initDatePicker.call(target)
+  })
 }
 
 export default { register }
